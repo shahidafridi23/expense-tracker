2d07dc3625a1a0d27e9809aa20a2a644f5343394
Shahid Afridi <shafridi9807@gmail.com>
2024-07-17 13:47:10 +0530

    added error handler

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import "dotenv/config";
+import "express-async-errors";
 import express from "express";
 import connectDB from "./db/connect.js";
 import authRouter from "./routes/auth.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
 
 const app = express();
 
@@ -15,6 +17,7 @@ app.use(express.json());
 //routes
 app.use("/api/auth", authRouter);
 
+app.use(errorHandlerMiddleware);
 //server
 const port = process.env.PORT || 3000;
 
