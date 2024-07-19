2812756c4ae08da63967556bb5c6aca442522d3d
Shahid Afridi <shafridi9807@gmail.com>
2024-07-19 22:31:47 +0530

    fixing bugs

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
+  optimizeDeps: {
+    include: ["@tanstack/react-query"],
+  },
   plugins: [react()],
   resolve: {
     alias: {
